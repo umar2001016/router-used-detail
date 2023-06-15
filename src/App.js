263fc68592ee51a4cbbd85ec4ea6debb0e4d712f
umar hussain <bsf2001016@ue.edu.pk>
2023-06-15 06:21:11 -0700

    detail router
    vdfdf

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import {
+  createBrowserRouter, 
+  createRoutesFromElements,
+  Route, 
+  RouterProvider
+} from 'react-router-dom'
+
+// pages
+import Home from './pages/Home'
+import About from './pages/About'
+import Faq from './pages/help/Faq'
+import Contact from './pages/help/Contact'
+import NotFound from './pages/NotFound'
+import Careers, { careersLoader } from './pages/careers'
+
+// layouts
+import RootLayout from './layouts/RootLayout'
+import HelpLayout from './layouts/HelpLayout'
+import CareersLayout from './layouts/careerLayout'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="help" element={<HelpLayout />}>
+        <Route path="faq" element={<Faq />} />
+        <Route path="contact" element={<Contact/>} />
+      </Route>
+      <Route path="careers" element={<CareersLayout/>}>
+        <Route 
+          index 
+          element={<Careers />} 
+          loader={careersLoader} 
+        />
+      </Route>
+
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+)
+
+function App() {
+  return (
+    <RouterProvider router={router} />
+  );
+}
+
+export default App
